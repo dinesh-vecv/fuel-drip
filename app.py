7dc99ca6fc7e9b578ddac1b2e5ed3903b8d05e3d
dinesh-vecv <159434720+dinesh-vecv@users.noreply.github.com>
2024-03-17 14:26:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 import folium
 from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
 
